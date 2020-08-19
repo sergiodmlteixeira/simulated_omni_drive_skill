61993db62fe44bce993ebabd09594617763b78e9
Sérgio Daniel Marinho <sergio.d.marinho@inesctec.pt>
2020-08-19 17:52:43 +0100

    Changed before_script

--- .ci_scripts/before_script.sh
+++ .ci_scripts/before_script.sh
@@ -1,10 +1,17 @@
-# start SSH agent
-# shellcheck disable=SC2046
-eval $(ssh-agent -s)
-# add key to agent
-ssh-add <(echo "$SSH_PRIVATE_KEY") || { res=$?; echo "could not add ssh key"; exit $res; }
-if [ -n "$SSH_SERVER_HOSTKEYS" ]; then
-  mkdir -p ~/.ssh
-  # setup known hosts
-  echo "$SSH_SERVER_HOSTKEYS" > ~/.ssh/known_hosts
-fi
+## start SSH agent
+## shellcheck disable=SC2046
+#eval $(ssh-agent -s)
+## add key to agent
+#ssh-add <(echo "$SSH_PRIVATE_KEY") || { res=$?; echo "could not add ssh key"; exit $res; }
+#if [ -n "$SSH_SERVER_HOSTKEYS" ]; then
+#  mkdir -p ~/.ssh
+#  # setup known hosts
+#  echo "$SSH_SERVER_HOSTKEYS" > ~/.ssh/known_hosts
+#fi
+mkdir .ssh/
+echo -e "$SSH_PRIVATE_KEY" > .ssh/id_ed25519
+chmod 600 .ssh/id_ed25519
+
+touch .ssh/known_hosts
+ssh-keyscan github.com >> .ssh/known_hosts
+ssh-keyscan gitlab.inesctec.pt >> .ssh/known_hosts
