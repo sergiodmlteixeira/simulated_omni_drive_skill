66552b1051fb60d6741ed7d95bfe899fe2cff628
Sérgio Daniel Marinho <sergio.d.marinho@inesctec.pt>
2020-08-20 16:05:36 +0100

    Test script

--- .ci_scripts/aws_init.sh
+++ .ci_scripts/aws_init.sh
@@ -3,7 +3,14 @@
 aws_main()
 {
 
-    env
+    eval $(ssh-agent -s)
+    # add key to agent
+    ssh-add <(echo "$SSH_PRIVATE_KEY" | base64 -d) || { res=$?; echo "could not add ssh key"; exit $res; }
+    if [ -n "$SSH_SERVER_HOSTKEYS" ]; then
+      mkdir -p ~/.ssh
+      # setup known hosts
+      (echo "$SSH_SERVER_HOSTKEYS" | base64 -d)  > ~/.ssh/known_hosts
+    fi
 
     if [ -f "~/.ssh/known_hosts" ]
     then
