a1f2a04995159edb57587604c8bd7922bd313c6a
Sérgio Daniel Marinho <sergio.d.marinho@inesctec.pt>
2020-08-21 10:27:09 +0100

    Test fix

--- .ci_scripts/before_script.sh
+++ .ci_scripts/before_script.sh
@@ -2,9 +2,9 @@ apt-get -qq install -y --no-upgrade --no-install-recommends ssh ssh-client
 
 eval $(ssh-agent -s)
 # add key to agent
-ssh-add <(echo "$SSH_PRIVATE_KEY" | base64 -d) || { res=$?; echo "could not add ssh key"; exit $res; 
+ssh-add <(echo "$SSH_PRIVATE_KEY" | base64 -d) || { res=$?; echo "could not add ssh key"; exit $res; }
 mkdir -p ~/.ssh
 # setup known hosts
 chmod 700 ~/.ssh
-(echo "$SSH_SERVER_HOSTKEYS" | base64 -d) > ~/.ssh/known_hosts
+(echo "$SSH_SERVER_HOSTKEYS" | base64 -d)  > ~/.ssh/known_hosts
 chmod 644 ~/.ssh/known_hosts
