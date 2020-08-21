df4cc441288ab22ffc71c55e99a14544dac0ab5b
Sérgio Daniel Marinho <sergio.d.marinho@inesctec.pt>
2020-08-21 10:10:49 +0100

    Test fix

--- .ci_scripts/before_script.sh
+++ .ci_scripts/before_script.sh
@@ -1,21 +1,10 @@
-# start SSH agent
-# shellcheck disable=SC2046
+apt-get -qq install -y --no-upgrade --no-install-recommends ssh ssh-client
+
 eval $(ssh-agent -s)
 # add key to agent
-ssh-add <(echo "$SSH_PRIVATE_KEY" | base64 -d) || { res=$?; echo "could not add ssh key"; exit $res; }
-if [ -n "$SSH_SERVER_HOSTKEYS" ]; then
-  mkdir -p ~/.ssh
-  # setup known hosts
-  (echo "$SSH_SERVER_HOSTKEYS" | base64 -d)  > ~/.ssh/known_hosts
-fi
-
-
-
-#eval $(ssh-agent -s)
-#ssh-add <(echo "$SSH_PRIVATE_KEY" | base64 -d)
-#
-#mkdir -p ~/.ssh
-#chmod 700 ~/.ssh
-#ssh-keyscan gitlab.inesctec.pt >> ~/.ssh/known_hosts
-#chmod 644 ~/.ssh/known_hosts
-
+ssh-add <(echo "$SSH_PRIVATE_KEY" | base64 -d) || { res=$?; echo "could not add ssh key"; exit $res; 
+mkdir -p ~/.ssh
+# setup known hosts
+chmod 700 ~/.ssh
+(echo "$SSH_SERVER_HOSTKEYS" | base64 -d)  > ~/.ssh/known_hosts
+chmod 644 ~/.ssh/known_hosts
